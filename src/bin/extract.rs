d9ca9ed45901e86ea5c568d26473326af339e986
Loren M. Lang <lorenl@north-winds.org>
2019-03-16 11:33:07 -0700

    Ignore invalid directories and support symlinks

--- src/bin/extract.rs
+++ src/bin/extract.rs
@@ -7,33 +7,62 @@ use std::cell::{Ref, RefCell};
 use std::fs::File;
 use std::path::Path;
 use std::fs::{self, OpenOptions};
+use std::os::unix::fs::symlink;
 
 use hfsplus::*;
 
 fn extract<P: AsRef<Path>>(vol2: &Ref<HFSVolume<File>>, volume: Rc<RefCell<HFSVolume<File>>>, record: &CatalogRecord, local_parent: P) -> std::io::Result<()> {
     let filename = record.key.node_name.to_string().nfc().collect::<String>();
     let local_file_path = local_parent.as_ref().join(&filename);
+    if record.key.node_name.to_string().chars().nth(0) == Some('\u{0000}') {
+        return Ok(())
+    }
     match record.body {
         CatalogBody::Folder(body) => {
             fs::create_dir(&local_file_path)?;
             let children = vol2.get_children_id(body.folderID)?;
             for child in children {
+                println!("Extract...");
                 extract(&vol2, Rc::clone(&volume), child.as_ref(), &local_file_path)?;
-                let perms = match child.body {
-                    CatalogBody::Folder(ref x) => Some((&x.permissions, 4096)),
-                    CatalogBody::File(ref x) => Some((&x.permissions, x.dataFork.logicalSize)),
-                    _ => None,
-                };
             }
         },
         CatalogBody::File(body) => {
             println!("Found a file: {}", filename);
             let mut data_fork = Fork::load(Rc::clone(&vol2.file), Rc::clone(&volume), &body.dataFork)?;
             let data = data_fork.read_all().unwrap();
-            let mut local_file = OpenOptions::new().write(true)
-                                                   .create_new(true)
-                                                   .open(local_file_path)?;
-            local_file.write(&data[..]);
+            match body.permissions.fileMode & S_IFMT {
+                S_IFIFO => {
+                    println!("Found a FIFO!");
+                },
+                S_IFCHR => {
+                    println!("Found a Character device!");
+                },
+                S_IFDIR => {
+                    panic!("Found a directory inside a File object");
+                },
+                S_IFBLK => {
+                    println!("Found a Block device!");
+                },
+                S_IFREG => {
+                    println!("Saving contents to {:?}", local_file_path);
+                    let mut local_file = OpenOptions::new().write(true)
+                                                           .create_new(true)
+                                                           .open(local_file_path)?;
+                    println!("Saving...");
+                    local_file.write(&data[..])?;
+                    println!("Done.");
+                },
+                S_IFLNK => {
+                    println!("Found a symlink!");
+                    symlink(std::str::from_utf8(&data).unwrap(), local_file_path)?;
+                },
+                S_IFSOCK => {
+                    println!("Found a Socket!");
+                },
+                _ => {
+                    println!("Unsupported file type");
+                },
+            };
             //let contents = std::str::from_utf8(data.as_ref()).unwrap();
             //println!("Contents: {}", contents);
         },
@@ -57,7 +86,7 @@ fn main() -> std::io::Result<()> {
         writeln!(std::io::stderr(), "Destination for extraction must be a directory");
         std::process::exit(1);
     }
-    extract(&vol2, Rc::clone(&volume), &record, &local);
+    extract(&vol2, Rc::clone(&volume), &record, &local)?;
     //if let Some(p) = path {
     //    println!("Open path: {}", p);
     //    let record = vol2.get_path_record(&p)?;
